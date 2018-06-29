de82cf148d38fc58cf9dc8776e626ef51f0d373f
MD Hafijul Islam <MD Hafijul Islam@HRIDOY-PC>
2018-06-30 00:33:08 +0600

    Initialization of Interest Calculator

--- 06EmployeeSalaryCalculator/SalaryCalculator.Designer.cs
+++ 06EmployeeSalaryCalculator/SalaryCalculator.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SalaryCalculator));
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(173, 280);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(235, 63);
+            this.calculateButton.TabIndex = 14;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryTextBox.Location = new System.Drawing.Point(47, 212);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(522, 39);
+            this.salaryTextBox.TabIndex = 13;
+            this.salaryTextBox.TextChanged += new System.EventHandler(this.salaryTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Enter salary of Employee";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 55);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Salary Calculator";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(47, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SalaryCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(614, 380);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SalaryCalculator";
+            this.Text = "Employee Salary Calculator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SalaryCalculator_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- 06EmployeeSalaryCalculator/SalaryCalculator.cs
+++ 06EmployeeSalaryCalculator/SalaryCalculator.cs
@@ -16,5 +16,81 @@ namespace _06EmployeeSalaryCalculator
         {
             InitializeComponent();
         }
+
+        private string programTitle = "Salary Calculator";
+        private void salaryTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string temp = salaryTextBox.Text;
+
+            foreach (char c in temp)
+            {
+                if (Char.IsDigit(c) == false && c != '.')
+                {
+                    MessageBox.Show("Only Numbers are Allowed.", programTitle);
+                    return;
+                }
+            }
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            string salary = salaryTextBox.Text;
+
+            string replyMessage = String.Empty;
+
+            if (salary.Length == 0)
+            {
+                replyMessage = "Input Stream is Empty.";
+
+            }
+
+            else
+            {
+                double empSalary = Convert.ToDouble(salaryTextBox.Text);
+                double bonus = 0.0;
+                int rate = 0;
+
+                if (empSalary > 10000)
+                {
+                    bonus = (empSalary * 5) / 100;
+                    rate = 5;
+                }
+
+                else if (empSalary <= 10000 && empSalary >= 8000)
+                {
+                    bonus = (empSalary * 6) / 100;
+                    rate = 6;
+                }
+
+                else
+                {
+                    bonus = (empSalary * 7) / 100;
+                    rate = 7;
+                }
+
+                replyMessage = "Salary Bonus Information\n" +
+                               "\nEmployee Salary: " + empSalary.ToString() +
+                               "\nBonus Rate : " + rate.ToString() +
+                               "\nSalary Bonus : " + bonus.ToString();
+            }
+
+            MessageBox.Show(replyMessage, programTitle);
+        }
+
+        private void SalaryCalculator_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var dialogResult = MessageBox.Show("Are you sure to Quit!", programTitle,
+                MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            else
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
