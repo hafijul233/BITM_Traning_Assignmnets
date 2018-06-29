4de7f6d6568c9fa9c2535b74d2e71a1f7ff861d4
MD Hafijul Islam <MD Hafijul Islam@HRIDOY-PC>
2018-06-29 22:41:59 +0600

    Unicode Filter Add

--- 04SCIITeller/ASCIITeller.Designer.cs
+++ 04SCIITeller/ASCIITeller.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ASCIITeller));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.charactertextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(70, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ASCII Teller";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter a single character";
+            // 
+            // charactertextBox
+            // 
+            this.charactertextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charactertextBox.Location = new System.Drawing.Point(86, 202);
+            this.charactertextBox.Name = "charactertextBox";
+            this.charactertextBox.Size = new System.Drawing.Size(392, 39);
+            this.charactertextBox.TabIndex = 3;
+            this.charactertextBox.TextChanged += new System.EventHandler(this.charactertextBox_TextChanged);
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(162, 275);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(235, 63);
+            this.checkButton.TabIndex = 4;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = false;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // ASCIITeller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 344);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(585, 377);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.charactertextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ASCIITeller";
             this.Text = "ASCII Teller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ASCIITeller_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox charactertextBox;
+        private System.Windows.Forms.Button checkButton;
     }
 }
 
--- 04SCIITeller/ASCIITeller.cs
+++ 04SCIITeller/ASCIITeller.cs
@@ -16,5 +16,60 @@ namespace _04SCIITeller
         {
             InitializeComponent();
         }
+
+        private string programTitle = "ASCI TELLER";
+
+        private void charactertextBox_TextChanged(object sender, EventArgs e)
+        {
+            string temp = charactertextBox.Text;
+            if (temp.Length == 0) { }
+
+            else if (temp.Length > 1)
+                MessageBox.Show("Only Enter a Single Character.", programTitle);
+
+            else
+            {
+                char character = Convert.ToChar(temp);
+
+                if ((int) character >= 256)
+                {
+                    MessageBox.Show("ASCII Table Doesn't support this Character.", programTitle);
+
+                    charactertextBox.Text = "";
+                }
+            }
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string inputString = charactertextBox.Text;
+
+            string replyMessage = String.Empty;
+
+            if (inputString == "")
+            {
+                replyMessage = "Input Stream is Empty";
+            }
+
+            else
+            {
+                char character = Convert.ToChar(inputString);
+
+                int asciiValue = (int) character;
+
+                replyMessage = $"ASCII Value of \"{character}\" is : {asciiValue}.";
+
+            }
+
+            MessageBox.Show(replyMessage, programTitle);
+
+            charactertextBox.Text = "";
+
+        }
+
+        private void ASCIITeller_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
     }
 }
