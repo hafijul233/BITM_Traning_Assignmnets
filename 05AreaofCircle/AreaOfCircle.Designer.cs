ca2f8796056fd8c814ad433f40e0f1be90aec9a8
MD Hafijul Islam <MD Hafijul Islam@HRIDOY-PC>
2018-06-29 22:50:56 +0600

    Circle Wizard Program initialization

---
+++ 05AreaofCircle/AreaOfCircle.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _05AreaofCircle
+{
+    partial class AreaOfCircle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AreaOfCircle));
+            this.checkButton = new System.Windows.Forms.Button();
+            this.charactertextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(147, 275);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(235, 63);
+            this.checkButton.TabIndex = 9;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = false;
+            // 
+            // charactertextBox
+            // 
+            this.charactertextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charactertextBox.Location = new System.Drawing.Point(71, 202);
+            this.charactertextBox.Name = "charactertextBox";
+            this.charactertextBox.Size = new System.Drawing.Size(392, 39);
+            this.charactertextBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(142, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter a single character";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 55);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Circle Wizard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AreaOfCircle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 450);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.charactertextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AreaOfCircle";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.TextBox charactertextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
