429aa77028be09a8e734b6f2107ffe5cb565185e
MD Hafijul Islam <MD Hafijul Islam@HRIDOY-PC>
2018-06-29 14:49:31 +0600

    Prime Finder Program Completed

--- BITM_Traning_Assignmnet1/PrimeFinder.Designer.cs
+++ BITM_Traning_Assignmnet1/PrimeFinder.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrimeFinder));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputnumbertextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(95, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Prime Finder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter a Integer Number: ";
+            // 
+            // inputnumbertextBox
+            // 
+            this.inputnumbertextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputnumbertextBox.Location = new System.Drawing.Point(71, 199);
+            this.inputnumbertextBox.Name = "inputnumbertextBox";
+            this.inputnumbertextBox.Size = new System.Drawing.Size(483, 39);
+            this.inputnumbertextBox.TabIndex = 3;
+            this.inputnumbertextBox.TextChanged += new System.EventHandler(this.inputnumbertextBox_TextChanged);
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkButton.Location = new System.Drawing.Point(192, 267);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(243, 61);
+            this.checkButton.TabIndex = 4;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = false;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // PrimeFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(656, 405);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.inputnumbertextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PrimeFinder";
+            this.Text = "Prime Finder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PrimeFinder_FormClosing);
+            this.Load += new System.EventHandler(this.PrimeFinder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputnumbertextBox;
+        private System.Windows.Forms.Button checkButton;
     }
 }
 
--- BITM_Traning_Assignmnet1/PrimeFinder.cs
+++ BITM_Traning_Assignmnet1/PrimeFinder.cs
@@ -16,5 +16,93 @@ namespace BITM_Traning_Assignmnet1
         {
             InitializeComponent();
         }
+
+        private string programTile = "Prime Finder";
+
+        private void PrimeFinder_Load(object sender, EventArgs e)
+        {
+            inputnumbertextBox.Text = "0";
+        }
+
+        private void inputnumbertextBox_TextChanged(object sender, EventArgs e)
+        {
+            string temp = inputnumbertextBox.Text;
+            foreach (char c in temp)
+            {
+                if (Char.IsDigit(c) == false)
+                {
+                    MessageBox.Show("Only Numbers are Allowed.",programTile);
+                }
+            }
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string stringNumber = inputnumbertextBox.Text;
+
+            string replyMessage = String.Empty;
+            int proof = 0;
+
+            if (stringNumber.Length > 19)
+            {
+                MessageBox.Show("Input is Too BIG for Calculation.", programTile);
+            }
+
+            else if (stringNumber == "0" || stringNumber == "1")
+            {
+                replyMessage = stringNumber + " is not a Prime Number.";
+            }
+
+            else if (stringNumber == "2")
+            {
+                replyMessage = stringNumber + " is a Prime Number.";
+            }
+
+            else
+            {
+                ulong integerNumber = Convert.ToUInt64(stringNumber);
+                ulong range = (ulong)Math.Sqrt(integerNumber);
+
+                for (ulong i = 2; i <= range; i++)
+                {
+                    if (integerNumber % i == 0)
+                    {
+                        proof = 1;
+                        break;
+                    }
+                }
+
+                switch (proof)
+                {
+                    case 1:
+                        replyMessage = integerNumber.ToString() + " is not a Prime Number.";
+                        break;
+                    default:
+                        replyMessage = integerNumber.ToString() + " is a Prime Number.";
+                        break;
+                }
+
+                var dialogResult = MessageBox.Show(replyMessage, programTile, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    inputnumbertextBox.Text = String.Empty;
+                }
+            }
+        }
+
+        private void PrimeFinder_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var dialogResult = MessageBox.Show("Are you sure to Quit!", programTile, MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
