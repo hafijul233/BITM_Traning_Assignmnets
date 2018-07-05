1041a62007c886ba23830e98204bba6b14026c97
MD Hafijul Islam <MD Hafijul Islam@HRIDOY-PC>
2018-07-06 01:35:10 +0600

    Lab Xm Program Lunch Bill Calculator

---
+++ 13LabTestLunchBillCalculator/LunchBillCalculatorUi.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace _13LabTestLunchBillCalculator
+{
+    partial class LunchBillCalculatorUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.ShowNetBillButton = new System.Windows.Forms.Button();
+            this.ShowTotalButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.DiscounttextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TotalGrosstextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MeatUnittextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FishUnittextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RiceUnittextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(247, 435);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(109, 25);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "VAT (5%)";
+            // 
+            // ShowNetBillButton
+            // 
+            this.ShowNetBillButton.Location = new System.Drawing.Point(346, 499);
+            this.ShowNetBillButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ShowNetBillButton.Name = "ShowNetBillButton";
+            this.ShowNetBillButton.Size = new System.Drawing.Size(255, 52);
+            this.ShowNetBillButton.TabIndex = 16;
+            this.ShowNetBillButton.Text = "Show Net Bill";
+            this.ShowNetBillButton.UseVisualStyleBackColor = true;
+            this.ShowNetBillButton.Click += new System.EventHandler(this.ShowNetBillButton_Click);
+            // 
+            // ShowTotalButton
+            // 
+            this.ShowTotalButton.Location = new System.Drawing.Point(38, 306);
+            this.ShowTotalButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ShowTotalButton.Name = "ShowTotalButton";
+            this.ShowTotalButton.Size = new System.Drawing.Size(177, 52);
+            this.ShowTotalButton.TabIndex = 15;
+            this.ShowTotalButton.Text = "Show Total";
+            this.ShowTotalButton.UseVisualStyleBackColor = true;
+            this.ShowTotalButton.Click += new System.EventHandler(this.ShowTotalButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(604, 372);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "%";
+            // 
+            // DiscounttextBox
+            // 
+            this.DiscounttextBox.Location = new System.Drawing.Point(386, 368);
+            this.DiscounttextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DiscounttextBox.Name = "DiscounttextBox";
+            this.DiscounttextBox.Size = new System.Drawing.Size(194, 30);
+            this.DiscounttextBox.TabIndex = 13;
+            this.DiscounttextBox.TextChanged += new System.EventHandler(this.DiscounttextBox_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(260, 372);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(96, 25);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Discount";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(604, 308);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "tk";
+            // 
+            // TotalGrosstextBox
+            // 
+            this.TotalGrosstextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TotalGrosstextBox.Location = new System.Drawing.Point(386, 303);
+            this.TotalGrosstextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TotalGrosstextBox.Name = "TotalGrosstextBox";
+            this.TotalGrosstextBox.ReadOnly = true;
+            this.TotalGrosstextBox.Size = new System.Drawing.Size(194, 30);
+            this.TotalGrosstextBox.TabIndex = 10;
+            this.TotalGrosstextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(228, 308);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(124, 25);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Total Gross";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(594, 209);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Unit";
+            // 
+            // MeatUnittextBox
+            // 
+            this.MeatUnittextBox.Location = new System.Drawing.Point(386, 205);
+            this.MeatUnittextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MeatUnittextBox.Name = "MeatUnittextBox";
+            this.MeatUnittextBox.Size = new System.Drawing.Size(194, 30);
+            this.MeatUnittextBox.TabIndex = 7;
+            this.MeatUnittextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.MeatUnittextBox.TextChanged += new System.EventHandler(this.MeatUnittextBox_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 209);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Meat (Unit price: 100 tk)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(594, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Unit";
+            // 
+            // FishUnittextBox
+            // 
+            this.FishUnittextBox.Location = new System.Drawing.Point(386, 131);
+            this.FishUnittextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FishUnittextBox.Name = "FishUnittextBox";
+            this.FishUnittextBox.Size = new System.Drawing.Size(194, 30);
+            this.FishUnittextBox.TabIndex = 4;
+            this.FishUnittextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.FishUnittextBox.TextChanged += new System.EventHandler(this.FishUnittextBox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 136);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fish (Unit price: 80 tk)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(594, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Unit";
+            // 
+            // RiceUnittextBox
+            // 
+            this.RiceUnittextBox.Location = new System.Drawing.Point(386, 63);
+            this.RiceUnittextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.RiceUnittextBox.Name = "RiceUnittextBox";
+            this.RiceUnittextBox.Size = new System.Drawing.Size(194, 30);
+            this.RiceUnittextBox.TabIndex = 1;
+            this.RiceUnittextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.RiceUnittextBox.TextChanged += new System.EventHandler(this.RiceUnittextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rice (Unit price: 20 tk)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.ShowNetBillButton);
+            this.groupBox1.Controls.Add(this.ShowTotalButton);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.DiscounttextBox);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.TotalGrosstextBox);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.MeatUnittextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.FishUnittextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.RiceUnittextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(30, 31, 30, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(678, 601);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bill For Lunch";
+            // 
+            // LunchBillCalculatorUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 645);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "LunchBillCalculatorUi";
+            this.Text = "LunchBillCalculatorUi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button ShowNetBillButton;
+        private System.Windows.Forms.Button ShowTotalButton;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox DiscounttextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TotalGrosstextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MeatUnittextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FishUnittextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox RiceUnittextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ 13LabTestLunchBillCalculator/LunchBillCalculatorUi.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13LabTestLunchBillCalculator
+{
+    public partial class LunchBillCalculatorUi : Form
+    {
+        private string _programTitle = "Lunch Bill Calculator";
+
+        private double _riceUnit = 0.0;
+
+        private double _fishUnit = 0.0;
+
+        private double _meatUnit = 0.0;
+
+        private double _totalPrice = 0.0;
+
+        private double _discount = 0.0;
+
+        private double _netBill = 0.0;
+
+        public LunchBillCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private double TotalPrice(double riceunit, double fishunit, double meatunit)
+        {
+            double totalprice = (riceunit * 20.00) +
+                                (fishunit * 80.00) +
+                                (meatunit * 100.00);
+
+            return totalprice;
+        }
+
+        private double NetPrice(double total, double discount)
+        {
+            double includingVat = 0.0;
+            double billAfterDiscount = 0.0;
+
+            includingVat = total + ((total * 5) / 100);//5% vat minus korchi
+
+            billAfterDiscount = includingVat - (includingVat * discount) / 100;
+
+            return billAfterDiscount;
+        }
+
+        private void StringValidation(string value)
+        {
+
+            foreach (char c in value)
+            {
+                if ((Char.IsDigit(c) == false) && c != '.')
+                {
+                    MessageBox.Show("Only Numbers and (.) are Allowed.");
+                }
+            }
+        }
+
+        private void RiceUnittextBox_TextChanged(object sender, EventArgs e)
+        {
+            StringValidation(RiceUnittextBox.Text);
+        }
+
+        private void FishUnittextBox_TextChanged(object sender, EventArgs e)
+        {
+            StringValidation(FishUnittextBox.Text);
+        }
+
+        private void MeatUnittextBox_TextChanged(object sender, EventArgs e)
+        {
+            StringValidation(MeatUnittextBox.Text);
+        }
+
+
+        private void DiscounttextBox_TextChanged(object sender, EventArgs e)
+        {
+            StringValidation(DiscounttextBox.Text);
+        }
+
+
+        private void ShowTotalButton_Click(object sender, EventArgs e)
+        {
+            string totalgross = String.Empty;
+
+            if (RiceUnittextBox.Text == String.Empty ||
+                FishUnittextBox.Text == String.Empty ||
+                MeatUnittextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Input Field Is Empty.", _programTitle);
+            }
+
+            else
+            {
+                _riceUnit = Convert.ToDouble(RiceUnittextBox.Text);
+
+                _fishUnit = Convert.ToDouble(FishUnittextBox.Text);
+
+                _meatUnit = Convert.ToDouble(MeatUnittextBox.Text);
+
+                _totalPrice = TotalPrice(_riceUnit, _fishUnit, _meatUnit);
+
+                totalgross = String.Format("{0:F1}", _totalPrice);
+
+                TotalGrosstextBox.Text = totalgross;
+
+            }
+        }
+
+        private void ShowNetBillButton_Click(object sender, EventArgs e)
+        {
+            _discount = Convert.ToDouble(DiscounttextBox.Text);
+
+            _netBill = NetPrice(_totalPrice, _discount);
+
+            string replyMessage = "Total Amount To Pay: " + _netBill.ToString();
+
+            MessageBox.Show(replyMessage, _programTitle);
+
+            RiceUnittextBox.Text = String.Empty;
+            FishUnittextBox.Text = String.Empty;
+            MeatUnittextBox.Text = String.Empty;
+
+            TotalGrosstextBox.Text = String.Empty;
+            DiscounttextBox.Text = String.Empty;
+        }
+    }
+}
