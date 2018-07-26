b2bd0e47359e64df07c63b21428f849a31935c69
Hafijul Islam <hafijul233@outlook.com>
2018-07-27 01:37:18 +0600

    correction

---
+++ StudentRegistrationBooth/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistrationBooth.Models
+{
+    class Customer
+    {
+        public string _code;
+        public string _name;
+        public string _contactNo;
+        public string _birthDate;
+        public string _address;
+        public string _subDistrict;
+        public string _district;
+
+        public void GetCustomerInfo(string code, string name, string contactNo, string birthDate,
+                                    string address, string subDistrict, string district)
+        {
+            _code = code;
+            _name = name;
+            _contactNo = contactNo;
+            _birthDate = birthDate;
+            _address = address;
+            _subDistrict = subDistrict;
+            _district = district;
+        }
+
+    }
+}
